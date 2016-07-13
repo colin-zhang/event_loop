88141a233c3d93b880ea2ef387cd81299906828c
colin-zhang <ingm@foxmail.com>
2016-07-13 23:48:26 +0800

    signalfd

---
+++ dev_signalfd.c
@@ -0,0 +1,63 @@
+#include "dev_event_def.h"
+#include <sys/signalfd.h>  
+#include <signal.h>  
+#include <unistd.h>  
+#include <stdlib.h>  
+#include <stdio.h>
+
+static int 
+dev_signalfd_handler(void *ptr)
+{
+    struct signalfd_siginfo fdsi;  
+    int s;
+    DEV_DECL_FD(ptr, fd);
+    s = read(sfd, &fdsi, sizeof(struct signalfd_siginfo));  
+    if (s != sizeof(struct signalfd_siginfo)) {
+        return -1;
+    }       
+
+    if (fdsi.ssi_signo == SIGINT) {  
+       printf("get SIGINT\n");  
+    } else if (fdsi.ssi_signo == SIGQUIT) {  
+       printf("get SIGQUIT\n");  
+       exit(EXIT_SUCCESS);  
+    } else {  
+       dbg_Print("unexpected signal\n");  
+    }  
+    
+    return 0;
+}
+
+dev_event_t* 
+dev_defualt_signalfd(void *data)
+{
+    dev_event_t *ev_ptr;
+    sigset_t mask;  
+    int sfd;  
+
+    sigemptyset(&mask);  
+    sigaddset(&mask, SIGINT);  
+    sigaddset(&mask, SIGQUIT);  
+
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) == -1)  
+       return NULL;
+
+    sfd = signalfd(-1, &mask, 0);  
+    if (sfd == -1) {
+        return NULL;
+    } 
+
+    ev_ptr = dev_event_creat(sfd, DEV_EVENT_SIG, EPOLLIN, 0);
+    if (ev_ptr == NULL) {
+        dbg_Print("dev_event_creat\n");
+        return NULL;
+    }
+
+    dev_event_set_data(ev_ptr, data, dev_signalfd_handler, NULL);
+
+    return ev_ptr;
+}
+
+
+  
+ 
---
+++ dev_signalfd.h
@@ -0,0 +1,6 @@
+#ifndef _DEV_SIGNALFD_H
+#define _DEV_SIGNALFD_H 
+
+dev_event_t* dev_defualt_signalfd(void *data);
+
+#endif
