0d51c212368cd5754c6529f56f1eb570d25284b4
colin-zhang <ingm@foxmail.com>
2016-07-14 14:21:36 +0800

    timer test

--- timer_test.c
+++ timer_test.c
@@ -10,8 +10,8 @@
 int 
 timer1_hander(void *ptr, void *ptr_self)
 {
-    fprintf(stdout, "timer ev:%s\n", (char *)ptr);
-    fprintf(stdout, "timer1_hander: %s\n", (char *)ptr_self);
+    fprintf(stdout, "timer ev:%s", (char *)ptr);
+    fprintf(stdout, "timer1_hander: %s", (char *)ptr_self);
     return 0;
 }
 
@@ -23,10 +23,19 @@ timer2_hander(void *ptr, void *ptr_self)
     return 0;
 }
 
+
+int 
+timer3_hander(void *ptr, void *ptr_self)
+{
+    fprintf(stdout, "timer3_hander: %s", (char *)ptr_self);
+    return 0;
+}
+
+
 int main(int argc, char const *argv[])
 {
     dev_event_t *timer_ev = NULL;
-    dev_timer_ev_t *timer1 = NULL, *timer2 = NULL;
+    dev_timer_ev_t *timer1 = NULL, *timer2 = NULL, *timer3 = NULL;
 
     if (dev_event_deafult_loop_init(100) == NULL) {
         fprintf(stderr, "Fail to create deafult loop\n");
@@ -34,7 +43,7 @@ int main(int argc, char const *argv[])
     }
 
     char *ev_ptr = malloc(512);
-    snprintf(ev_ptr, 512, "timer event\n");    
+    snprintf(ev_ptr, 512, "public\n");    
     timer_ev = dev_event_timer_creat(100, ev_ptr);
     if (timer_ev == NULL) {
         fprintf(stderr, "%s\n", "timer event creat error\n");
@@ -42,22 +51,30 @@ int main(int argc, char const *argv[])
     }
 
     char *timer1_ptr = malloc(512);
-    snprintf(timer1_ptr, 512, "timer 1 test \n");
-    timer1 = dev_sub_timer_creat(2.1, 0, timer1_hander, timer1_ptr);
+    snprintf(timer1_ptr, 512, "timer public\n");
+    timer1 = dev_sub_timer_creat(2.1, 5, timer1_hander, timer1_ptr);
     if (timer1 == NULL) {
         fprintf(stderr, "%s\n", "sub timer creat error\n");
         exit(-1);
     }
 
 
-    timer2 = dev_sub_timer_creat(5, 0, timer2_hander, NULL);
-    if (timer1 == NULL) {
+    timer2 = dev_sub_timer_creat(4.1, 0, timer2_hander, NULL);
+    if (timer2 == NULL) {
+        fprintf(stderr, "%s\n", "sub timer creat error\n");
+        exit(-1);
+    }
+
+
+    timer3 = dev_sub_timer_creat(4.1, 0, timer3_hander, timer1_ptr);
+    if (timer3 == NULL) {
         fprintf(stderr, "%s\n", "sub timer creat error\n");
         exit(-1);
     }
 
     dev_event_timer_add(timer_ev, timer1);
     dev_event_timer_add(timer_ev, timer2);
+    dev_event_timer_add(timer_ev, timer3);
    
     dev_event_loop_add(dev_event_deafult_loop(), timer_ev);
 
